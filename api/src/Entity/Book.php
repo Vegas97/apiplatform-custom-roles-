f18cf6eeb808cf9618121d75769bfe7cee49bba8
Diego Palo <dp0997@gmail.com>
2025-04-09 20:37:32 +0200

    Add PHPDoc blocks and rename id property to _id in Book and Review entities

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Book entity for the API Platform application.
+ *
+ * PHP version 8.1
+ *
+ * @category Entity
+ * @package  App\Entity
+ * @author   John Doe <john.doe@example.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @version  GIT: <git_id>
+ * @link     https://api-platform.com
+ */
 // api/src/Entity/Book.php
 namespace App\Entity;
 
@@ -7,46 +19,90 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/** A book. */
+/**
+ * Book entity class.
+ *
+ * @category Entity
+ * @package  App\Entity
+ * @author   John Doe <john.doe@example.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @version  GIT: <git_id>
+ * @link     https://api-platform.com
+ */
 #[ORM\Entity]
 #[ApiResource]
 class Book
 {
-    /** The ID of this book. */
+    /**
+     * The ID of this book.
+     */
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
-    private ?int $id = null;
+    private ?int $_id = null;
 
+    /**
+     * The ISBN of this book (or null if doesn't have one).
+     */
     #[ORM\Column(nullable: true)]
     #[Assert\Isbn]
     public ?string $isbn = null;
 
+    /**
+     * The title of this book.
+     */
     #[ORM\Column]
     #[Assert\NotBlank]
     public string $title = '';
 
+    /**
+     * The description of this book.
+     */
     #[ORM\Column(type: 'text')]
     #[Assert\NotBlank]
     public string $description = '';
 
+    /**
+     * The author of this book.
+     */
     #[ORM\Column]
     #[Assert\NotBlank]
     public string $author = '';
 
+    /**
+     * The publication date of this book.
+     */
     #[ORM\Column]
     #[Assert\NotNull]
     public ?\DateTimeImmutable $publicationDate = null;
 
-    /** @var Review[] Available reviews for this book. */
-    #[ORM\OneToMany(targetEntity: Review::class, mappedBy: 'book', cascade: ['persist', 'remove'])]
+    /**
+     * Available reviews for this book.
+     *
+     * @var Review[] Collection of review objects
+     */
+    #[ORM\OneToMany(
+        targetEntity: Review::class,
+        mappedBy: 'book',
+        cascade: ['persist', 'remove']
+    )]
     public iterable $reviews;
 
+    /**
+     * Constructor.
+     *
+     * Initializes the reviews collection.
+     */
     public function __construct()
     {
         $this->reviews = new ArrayCollection();
     }
 
+    /**
+     * Get the book ID.
+     *
+     * @return ?int The book ID or null if not set
+     */
     public function getId(): ?int
     {
-        return $this->id;
+        return $this->_id;
     }
 }
--- api/src/Entity/Review.php
+++ api/src/Entity/Review.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Review entity for the API Platform application.
+ *
+ * PHP version 8.1
+ *
+ * @category Entity
+ * @package  App\Entity
+ * @author   John Doe <john.doe@example.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @version  GIT: <git_id>
+ * @link     https://api-platform.com
+ */
 // api/src/Entity/Review.php
 namespace App\Entity;
 
@@ -6,42 +18,68 @@ use ApiPlatform\Metadata\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/** A review of a book. */
+/**
+ * Review entity class.
+ *
+ * @category Entity
+ * @package  App\Entity
+ * @author   John Doe <john.doe@example.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @version  GIT: <git_id>
+ * @link     https://api-platform.com
+ */
 #[ORM\Entity]
 #[ApiResource]
 class Review
 {
-    /** The ID of this review. */
+    /**
+     * The ID of this review.
+     */
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
-    private ?int $id = null;
+    private ?int $_id = null;
 
-    /** The rating of this review (between 0 and 5). */
+    /**
+     * The rating of this review (between 0 and 5).
+     */
     #[ORM\Column(type: 'smallint')]
     #[Assert\Range(min: 0, max: 5)]
     public int $rating = 0;
 
-    /** The body of the review. */
+    /**
+     * The body of the review.
+     */
     #[ORM\Column(type: 'text')]
     #[Assert\NotBlank]
     public string $body = '';
 
-    /** The author of the review. */
+    /**
+     * The author of the review.
+     */
     #[ORM\Column]
     #[Assert\NotBlank]
     public string $author = '';
 
-    /** The date of publication of this review.*/
+    /**
+     * The date of publication of this review.
+     */
     #[ORM\Column]
     #[Assert\NotNull]
     public ?\DateTimeImmutable $publicationDate = null;
 
-    /** The book this review is about. */
+    /**
+     * The book this review is about.
+     */
     #[ORM\ManyToOne(inversedBy: 'reviews')]
     #[Assert\NotNull]
     public ?Book $book = null;
 
+    /**
+     * Get the review ID.
+     *
+     * @return ?int The review ID or null if not set
+     */
     public function getId(): ?int
     {
-        return $this->id;
+        return $this->_id;
     }
 }
