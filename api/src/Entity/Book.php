cbf6b367c644f7e6101fc000af5daade24bceaf3
Diego Palo <dp0997@gmail.com>
2025-04-09 18:19:34 +0200

    Add validation constraints to Book and Review entities

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Metadata\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /** A book. */
 #[ORM\Entity]
@@ -15,25 +16,25 @@ class Book
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     private ?int $id = null;
 
-    /** The ISBN of this book (or null if doesn't have one). */
     #[ORM\Column(nullable: true)]
-    public ?string $isbn = null;
+    #[Assert\Isbn]
+     public ?string $isbn = null;
 
-    /** The title of this book. */
-    #[ORM\Column]
-    public string $title = '';
+     #[ORM\Column]
+    #[Assert\NotBlank]
+     public string $title = '';
 
-    /** The description of this book. */
-    #[ORM\Column(type: 'text')]
-    public string $description = '';
+     #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank]
+     public string $description = '';
 
-    /** The author of this book. */
-    #[ORM\Column]
-    public string $author = '';
+     #[ORM\Column]
+    #[Assert\NotBlank]
+     public string $author = '';
 
-    /** The publication date of this book. */
-    #[ORM\Column]
-    public ?\DateTimeImmutable $publicationDate = null;
+     #[ORM\Column]
+    #[Assert\NotNull]
+     public ?\DateTimeImmutable $publicationDate = null;
 
     /** @var Review[] Available reviews for this book. */
     #[ORM\OneToMany(targetEntity: Review::class, mappedBy: 'book', cascade: ['persist', 'remove'])]
