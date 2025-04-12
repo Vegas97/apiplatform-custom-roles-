e64ddeba1ea5270116d2b6305f96f4ecdcaf7560
Diego Palo <dp0997@gmail.com>
2025-04-12 10:10:13 +0200

    fix test with proper assertions

--- tests/Api/GuestReservationDtoTest.php
+++ tests/Api/GuestReservationDtoTest.php
@@ -87,7 +87,7 @@ class GuestReservationDtoTest extends WebTestCase
      * 
      * @return void
      */
-    public function testGetItemWithPortalAndRoles(): void
+    public function testGetItemsWithPortalAndRoles(): void
     {
         // todo: 1 fix if portal is not something valid, ruturn 404 not found
         // todo: 2 fix merge logic (so if you have more then 1 entityMapping )
@@ -160,15 +160,6 @@ class GuestReservationDtoTest extends WebTestCase
 
         $jsonResponse = json_decode($responseContent, true);
 
-        dd($jsonResponse);
-
-        // Make sure we have hydra:member in the response (collection)
-        $this->assertArrayHasKey('hydra:member', $jsonResponse, 'Response should have hydra:member key');
-        $this->assertNotEmpty($jsonResponse['hydra:member'], 'Collection should not be empty');
-
-        // Get the first item from the collection for testing
-        $jsonResponse = $jsonResponse['hydra:member'][0] ?? [];
-
         // Log the response data using the same logger instance
         $logger->info('GuestReservationDto item test response received', [
             'statusCode' => $client->getResponse()->getStatusCode(),
@@ -176,22 +167,55 @@ class GuestReservationDtoTest extends WebTestCase
             'responseData' => $jsonResponse
         ]);
 
-        // Assert that fields accessible to distributor portal are present
-        $this->assertArrayHasKey('id', $jsonResponse, 'Response should have id field');
-        $this->assertArrayHasKey('reservationId', $jsonResponse, 'Response should have reservationId field');
-        $this->assertArrayHasKey('name', $jsonResponse, 'Response should have name field');
+        // Assert the structure of the response
+        $this->assertArrayHasKey('@context', $jsonResponse, 'Response should have @context key');
+        $this->assertArrayHasKey('@id', $jsonResponse, 'Response should have @id key');
+        $this->assertArrayHasKey('@type', $jsonResponse, 'Response should have @type key');
+        $this->assertArrayHasKey('totalItems', $jsonResponse, 'Response should have totalItems key');
+        $this->assertArrayHasKey('member', $jsonResponse, 'Response should have member key');
 
-        // These fields may not be present in the accessible fields based on the logs
-        // The logs show only id, reservationId, and name are accessible
-        // $this->assertArrayHasKey('nationality', $jsonResponse, 'Response should have nationality field');
-        // $this->assertArrayHasKey('checkInDate', $jsonResponse, 'Response should have checkInDate field');
-        // $this->assertArrayHasKey('checkOutDate', $jsonResponse, 'Response should have checkOutDate field');
+        // Assert the values of the response
+        $this->assertEquals('/api/contexts/GuestReservationDto', $jsonResponse['@context'], '@context should be /api/contexts/GuestReservationDto');
+        $this->assertEquals('/api/guest_reservation_dtos', $jsonResponse['@id'], '@id should be /api/guest_reservation_dtos');
+        $this->assertEquals('Collection', $jsonResponse['@type'], '@type should be Collection');
+        $this->assertEquals(3, $jsonResponse['totalItems'], 'totalItems should be 3');
+        $this->assertCount(3, $jsonResponse['member'], 'member should have 3 items');
 
+        // Assert the structure of each member item in the collection
+        foreach ($jsonResponse['member'] as $member) {
+            $this->assertArrayHasKey('@id', $member, 'Member should have @id key');
+            $this->assertArrayHasKey('@type', $member, 'Member should have @type key');
+            $this->assertArrayHasKey('id', $member, 'Member should have id key');
+            $this->assertArrayHasKey('reservationId', $member, 'Member should have reservationId key');
 
-        // Assert that email is not present (restricted from distributor portal)
-        $this->assertArrayNotHasKey('email', $jsonResponse, 'Response should NOT have email field for distributor portal');
-        $this->assertArrayNotHasKey('birthDate', $jsonResponse, 'Response should NOT have birthDate field for distributor portal');
-        $this->assertArrayNotHasKey('roomNumber', $jsonResponse, 'Response should NOT have roomNumber field for distributor portal');
+            // Assert that each member has exactly 4 fields
+            $this->assertCount(4, $member, 'Each member should have exactly 4 fields');
+
+            // Assert that the @type is GuestReservationDto
+            $this->assertEquals('GuestReservationDto', $member['@type'], 'Member @type should be GuestReservationDto');
+
+            // Assert that email is not present (restricted fields)
+            $this->assertArrayNotHasKey('email', $member, 'Member should NOT have email field');
+            $this->assertArrayNotHasKey('birthDate', $member, 'Member should NOT have birthDate field');
+            $this->assertArrayNotHasKey('roomNumber', $member, 'Member should NOT have roomNumber field');
+            $this->assertArrayNotHasKey('name', $member, 'Member should NOT have name field');
+            $this->assertArrayNotHasKey('nationality', $member, 'Member should NOT have nationality field');
+            $this->assertArrayNotHasKey('checkInDate', $member, 'Member should NOT have checkInDate field');
+            $this->assertArrayNotHasKey('checkOutDate', $member, 'Member should NOT have checkOutDate field');
+        }
+
+        // Assert specific values for each member
+        $this->assertEquals('/api/guest_reservation_dtos/G001', $jsonResponse['member'][0]['@id']);
+        $this->assertEquals('G001', $jsonResponse['member'][0]['id']);
+        $this->assertEquals('R101', $jsonResponse['member'][0]['reservationId']);
+
+        $this->assertEquals('/api/guest_reservation_dtos/G002', $jsonResponse['member'][1]['@id']);
+        $this->assertEquals('G002', $jsonResponse['member'][1]['id']);
+        $this->assertEquals('R102', $jsonResponse['member'][1]['reservationId']);
+
+        $this->assertEquals('/api/guest_reservation_dtos/G003', $jsonResponse['member'][2]['@id']);
+        $this->assertEquals('G003', $jsonResponse['member'][2]['id']);
+        $this->assertEquals('R103', $jsonResponse['member'][2]['reservationId']);
 
         // Log the test completion using the same logger instance
         $logger->info('GuestReservationDto item test completed', [
