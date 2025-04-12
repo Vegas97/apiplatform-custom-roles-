71de14b8cac1f28353f9543ea7b4091216d17fa3
Diego Palo <dp0997@gmail.com>
2025-04-12 10:04:37 +0200

    fix logs +

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -13,6 +13,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,6 +30,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HealthController extends AbstractController
 {
     /**
+     * Logger instance.
+     *
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
+
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $logger The logger service
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
      * Simple health check endpoint that returns a 200 status code.
      *
      * @return JsonResponse The health status response
@@ -38,10 +56,17 @@ class HealthController extends AbstractController
     {
         $timestamp = new \DateTime();
         
+        // Test log entry to verify logging is working
+        $this->logger->info('Health check endpoint accessed', [
+            'timestamp' => $timestamp->format('Y-m-d H:i:s'),
+            'environment' => $this->getParameter('kernel.environment')
+        ]);
+        
         return new JsonResponse(
             [
                 'status' => 'ok',
                 'timestamp' => $timestamp,
+                'loggingTest' => 'Log entry created - check /var/logs directory'
             ]
         );
     }
